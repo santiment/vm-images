49da60f58811d503be8ccdfe47c9b316a071f213
Tzanko Matev <tsanko@gmail.com>
2017-05-21 08:04:36 +0000

    Remove nixpkgs submodule and fix ami creation
    The previous ami creation left the temporary ssh key in the ami in a
    way which prevented us from using AWS facilites for providing a key
    during instance creation.

--- ami_install.sh
+++ ami_install.sh
@@ -13,14 +13,14 @@ cat >/etc/nixos/configuration.nix <<EOF
 {config, pkgs, ...}:
 {
   imports = [./platform.nix];
-
-  services.openssh.enable = true;
-  users.extraUsers.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLdgzhoLTiW1bEwuwJX0E1kCIZT0ZbCk/jtnbynbtPAaM2ARX66MwK+dGZrEfK51b2OyBf7ZV4AbdeRpR/KXWkwV1eJcbP7Tv07mCorw2YzKq5hEkQasoKMoOlLR78cRNOnsOGn/c5vZNAiX8S4x1efRnkq9EAKrtcCR5McqcFn/47UTy9n+FkABuST2o+Vdw+tYWvF2xLBNgeFsRd3WzZrZs7YFUTlamv3PZ7fQhvgK2auwxYb+6aAxkRU1q+K0aEhZ9myj0S1SF0A2Tz4ebhcPUde/Gdn5BHQJToQoxBYW4taJHNnd616MqjLCWuod48SxwlpwdPDtVcWpKUxN2x vagrant@nixbox" ];
 }
 EOF
 
 nix-channel --update
 nixos-rebuild switch
 
-#Remove old ssh keys
+# Remove old ssh keys used by packer. Otherwise our instance will not
+# be able to read the key provided by amazon during instance creation
 rm -rf /root/.ssh
+# For the same reason, remove old ec2 metadata
+rm -rf /etc/ec2-metadata
